493733ad4cd87ec4deb274ee7e9b3190100b84c7
DanLeLo <dalo6@student.eadania.dk>
2025-09-30 10:58:24 +0200

    Add project files.

---
+++ GameHub - Afl/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GameHub___Afl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
